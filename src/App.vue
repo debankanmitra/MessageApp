2be22d7e316cf590f7cc1a8643a4c2e1edc8cf3d
debankanmitra <debankanmitra@gmail.com>
2024-07-24 10:50:14 +0530

    all

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+
+import useStore from "@src/store/store";
+import { fetchData } from "@src/store/defaults";
+
+import FadeTransition from "@src/components/ui/transitions/FadeTransition.vue";
+
+const store = useStore();
+
+// update localStorage with state changes
+store.$subscribe((_mutation, state) => {
+  localStorage.setItem("chat", JSON.stringify(state));
+});
+
+// here we load the data from the server.
+onMounted(async () => {
+  store.status = "loading";
+
+  // fake server call
+  setTimeout(() => {
+    store.delayLoading = false;
+  });
+  const request = await fetchData();
+
+  store.$patch({
+    status: "success",
+    user: request.data.user,
+    conversations: request.data.conversations,
+    notifications: request.data.notifications,
+    archivedConversations: request.data.archivedConversations,
+  });
+});
+
+// the app height
+const height = ref(`${window.innerHeight}px`);
+
+// change the app height to the window hight.
+const resizeWindow = () => {
+  height.value = `${window.innerHeight}px`;
+};
+
+// and add the resize event when the component mounts.
+onMounted(() => {
+  window.addEventListener("resize", resizeWindow);
+});
+
+// remove the event when un-mounting the component.
+onUnmounted(() => {
+  window.removeEventListener("resize", resizeWindow);
+});
+</script>
+
+<template>
+  <div :class="{ dark: store.settings.darkMode }">
+    <div
+      class="bg-white dark:bg-gray-800 transition-colors duration-500"
+      :style="{ height: height }"
+    >
+      <router-view v-slot="{ Component }">
+        <FadeTransition>
+          <component :is="Component" />
+        </FadeTransition>
+      </router-view>
+    </div>
+  </div>
+</template>
